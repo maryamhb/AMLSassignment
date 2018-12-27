b8bb6198b03ea73bb6a6aa5227412ec275bc37ab
Maryam Habibollahi <mh76110@gmail.com>
2018-12-27 12:27:11 +0000

    Histogram of Oriented Gradients landmark detection

--- Python/code/preprocessing.py
+++ Python/code/preprocessing.py
@@ -1,14 +1,111 @@
 import os
 import numpy as np
+from keras.preprocessing import image
+import cv2
+import dlib
 
 # Data directory
-img_path = os.path.join('..','dataset')
-labels_path = os.path.join('..','attribute_list.csv')
+img_dir = os.path.join('..','dataset')
+labels_dir = os.path.join('..','attribute_list.csv')
 
-# Load data
-images = [os.path.join(img_path, l) for l in os.listdir(img_path)]
-labels_file = open(labels_path, 'r')
-lines = labels_file.readlines()
+# Face detector + landmark predictor
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+
+# Run functions through dataset
+
+def label_features():
+    # load image + label data
+    img_paths = [os.path.join(img_dir, l) for l in os.listdir(img_dir)]
+    labels_file = open(labels_dir, 'r')
+    lines = labels_file.readlines()
+    human_labels = {line.split(',')[0] : int(line.split(',')[5]) for line in lines[2:]}
+
+    if os.path.isdir(img_dir):
+        all_features = []
+        all_labels = []
+        noise = []
+        counter = 0
+        for img_path in img_paths:
+            img_name = img_path.split('.')[2].split('/')[-1]
+
+            img = image.img_to_array(image.load_img(img_path,
+                                                    target_size=None,
+                                                    interpolation='bicubic'))
+            features, _ = get_landmarks(img)
+            if features is not None:
+                all_features.append(features)
+                all_labels.append(human_labels[img_name])
+            else:
+                noise.append(img_path)
+                counter += 1
+
+    landmark_features = np.array(all_features)
+    feature_labels = (np.array(all_labels))
+    print(counter, " noisy images:\n", noise)
+
+    return landmark_features, feature_labels
+
+
+# Localise face + predict shape using dlib and OpenCV
+# via a pre-trained Histogram of Oriented Gradients (HOG)
+
+def get_landmarks(img):
+    # resize image + convert to grayscale
+    img = img.astype('uint8')
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = gray.astype('uint8')
+
+    # localise faces in grayscale
+    rects = detector(gray, 1)
+    n_faces = len(rects)
+
+    if n_faces == 0:
+        return None, img
+
+    face_areas = np.zeros((1, n_faces))
+    face_shapes = np.zeros((136, n_faces), dtype=np.int64)
+
+    # loop through dlib's detections
+    for (i, rect) in enumerate(rects):
+        # apply shape predictor: get landmarks
+        temp_shape = predictor(gray, rect)
+        temp_shape = shape2np(temp_shape)
+
+        # convert dlib rect to bounding box
+        (x, y, w, h) = rect2bb(rect)
+        face_shapes[:, i] = np.reshape(temp_shape, [136])
+        face_areas[0,i] = w * h
+
+    # find + keep largest face
+    face_out = np.reshape(np.transpose(face_shapes[:, np.argmax(face_areas)]), [68, 2])
+
+    return face_out, img
+
+
+# Util functions
+
+def rect2bb(rect):
+    # convert dlib detector's rect to bounding box for convenience
+    x = rect.left()
+    y = rect.top()
+    w = rect.right() - x
+    h = rect.bottom() - y
+
+    return (x, y, w, h)
+
+
+def shape2np(shape, dtype="int"):
+    # init list of x,y coordinates
+    xy = np.zeros((shape.num_parts, 2), dtype=dtype)
+
+    # convert all landmarks to x,y 2-tuple
+    for i in range(shape.num_parts):
+        xy[i] = (shape.part(i).x, shape.part(i).y)
+
+    return xy
 
 
 '''
@@ -23,6 +120,11 @@ Pre-processing techniques:
  - Gamma correction (power-law equalisation)
  - Colour space transformation (RGB-LAB)
  
+Facial landmark detection:
+ - Holistic methods
+ - Constrained Local Model (CLM) methods
+ - Regression-based models 
+ 
 Feature extraction methods:
  - Haar-like features (Viola & Jones)
  - Histogram of Oriented Gradients (HOG)
@@ -30,3 +132,7 @@ Feature extraction methods:
  - Speeded Up Robust Feature (SURF)
  
 '''
+
+
+# Test on data
+label_features()
